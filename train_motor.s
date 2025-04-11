9c0f86a005615185424a68cb0d7de551abd4f63f
h_tye <harryijtye@gmail.com>
2025-04-11 14:47:41 -0400

    update train and door

--- train_motor.s
+++ train_motor.s
@@ -26,51 +26,7 @@ __main	PROC
 	;Registers used: r0, r1, r2, r7, r8, r9, r10, r11
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
-	; Enable Port C clocks
-	LDR r0, =RCC_BASE      			;load in base module
-	LDR r1, [r0, #RCC_AHB2ENR]     	;load in clock module
-	ORR r1, r1, #0x00000006         ;set clocks for Port B C high
-	STR r1, [r0, #RCC_AHB2ENR]     	;store result back to clock reg
-
-	; TRAIN
-	LDR r0, =GPIOC_BASE        		;load in base module
-	LDR r1, [r0, #GPIO_MODER]      	;load moad register
-	BIC r1, r1, #0x000C0000         ;clear bits for PC9, 18-19
-	ORR r1, r1, #0x00080000    		;set 01 for pin 9 for output
-	
-	BIC r1, r1, #0x00030000         ;clear bits for PC8, 16-17
-	ORR r1, r1, #0x00010000    		;set 01 for pin 8 for output
-	
-	BIC r1, r1, #0x00003000         ;clear bits for PC6, 12-13
-	ORR r1, r1, #0x00001000    		;set 01 for pin 6 for output
-	
-	BIC r1, r1, #0x00000C00         ;clear bits for PC5, 10-11
-	ORR r1, r1, #0x00000800    		;set 01 for pin 13 for output
-	STR r1, [r0,#GPIO_MODER]       	;store result back to moder
-	
-	;set the pupdr of PC9, PC8, PC6, PC5 
-	LDR r0, =GPIOC_BASE        		;load in base module
-	LDR r1, [r0, #GPIO_PUPDR]      	;load moad register
-	BIC r1, r1, #0x000C0000         ;clear bits for PC9, 18-19
-	ORR r1, r1, #0x00080000    		;set 01 for pin 9 for PU/PD
-	
-	BIC r1, r1, #0x00030000         ;clear bits for PC8, 16-17
-	ORR r1, r1, #0x00010000    		;set 01 for pin 8 for PU/PD
-	
-	BIC r1, r1, #0x00003000         ;clear bits for PC6, 12-13
-	ORR r1, r1, #0x00001000    		;set 01 for pin 6 for PU/PD
-	
-	BIC r1, r1, #0x00000C00         ;clear bits for PC5, 10-11
-	ORR r1, r1, #0x00000800    		;set 01 for pin 13 for PU/PD
-	STR r1, [r0,#GPIO_PUPDR]       	;store result back to pupdr
-	
-	
-	; Initialize all of the outputs to zero
-	LDR r0, =GPIOC_BASE
-	LDR r1, [r0, #GPIO_ODR]
-	BIC r1, r1, #0x00000360   		;clear bits 9,8,6,5
-	ORR r1, r1, #0x00000000
-	STR r1, [r0, #GPIO_ODR]
+	PUSH{r7,r8,r10,r11,r12} //want to listen on r9(direction)
 
 	BL main_loop
 
@@ -83,6 +39,8 @@ __main	PROC
 	MOV r9,  #0
     MOV r7,  #0
     MOV r8,  #0
+
+	POP{r7,r8,r10,r11,r12}
 
 	BX LR
 	
@@ -97,20 +55,11 @@ main_loop
 	MOVEQ r11, #0 					; Reset the rotation counter if we have reached 215 sequences
 	
 	;check to see what direction we have specified in main
-	LDR r0, =DIRECTION      		;placeholder for now, but flag for what direction to go
-	LDR r0, [=DIRECTION]    		
+	MOV r0, r9     					;load in flag for what direction to go    		
 	CMP r0, #1              		;compare flag with 1
 	BEQ full_step_cycle_forwards    ;if we have set flag to 1, then we want to initaite forward movement
 	CMP r0, #0						;compare flag with 0
 	BEQ full_step_cycle_forwards    ;if we have set flag to 0, initiate reverse movement
-	
-	;cleanup, set everything to 0 for safety
-	MOV r11, #0
-	MOV r10, #0
-	MOV r0,  #0
-	MOV r1,  #0
-	MOV r2,  #0
-	MOV r9,  #0
 	
 	BX LR                            ;exit once rotation is done
 	
